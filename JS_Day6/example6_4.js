4323d5dff04959e4c050a4315028628a2863c6e6
Zhao Xiaoyan <3556555480@qq.com>
2017-04-24 19:19:05 +0800

    Update example6_4.js

--- JS_Day6/example6_4.js
+++ JS_Day6/example6_4.js
@@ -1,7 +1,10 @@
-function bubble(arr){   //比较器函数
+var nums=[1,4,2,0,5,12];
+var emps=["Amy","Join","Kosn","Mand","Ksnk"];
+
+function bubble(arr,compare){   //比较器函数
       for(var n=1;n<arr.length;n++){
           for(var i=0;i<arr.length-n;i++){
-              if(arr[i]-arr[i+1]>0){
+              if(compare(arr[i],arr[i+1])>0){
                   var temp=arr[i];
                   arr[i]=arr[i+1];
                   arr[i+1]=temp;
@@ -10,5 +13,14 @@ function bubble(arr){   //比较器函数
       }
 }
 
-var nums=[1,4,2,0,5,12];
-bubble(nums);
+/*专门比较任意两个数字的大小*/
+function compareNum(a,b){return a-b;}
+
+/*比较任意两个字符串的大小*/
+function compareStr(a,b){
+     return a>b ?1 : a<b ?-1 :0;
+}
+
+
+bubble(nums,compareNum);  console.log(nums);
+bubble(emps,compareStr);  console.log(emps);
