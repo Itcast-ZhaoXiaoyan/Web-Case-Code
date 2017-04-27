15270c53854a4543665725c65069daf24573fca1
Zhao Xiaoyan <3556555480@qq.com>
2017-04-27 21:44:50 +0800

    Create 输入邮箱.js

---
+++ JS_Day7/输入邮箱.js
@@ -0,0 +1,14 @@
+function mail(email)
+{
+  //工具：2个
+  //indexOf查找字符的位置
+  //slice从某个位置开始，截取到某位结束
+
+    alert("用户名"+username);
+    alert("域名"+doamin);
+}
+
+
+<body>
+<button onclick="mail(prompt('输入邮箱'))">输入邮箱</button>
+</body>
