754073c9e74099cd7fd276817c72def48e5f4009
Zhao Xiaoyan <3556555480@qq.com>
2017-04-27 19:26:12 +0800

    每5位取一次子字符串

---
+++ JS_Day7/substr.js
@@ -0,0 +1,16 @@
+function decode(code)
+{
+    /*每5位取一次子字符串*/
+    /*从0开始，每隔5个字符遍历
+      同时创建空数组arr，从i开始，取5个字符——unicode
+      var char = String.fromCharCode(unicode)
+      将char追加到数组结尾
+      循环退出后，返回数组拼接后的结果
+    */
+    
+    for(var i=0,arr[];i<code.length;i+=5)
+    {
+        arr.push(String.fromCharCode(code.substr(i,5)));
+    }
+    return arr.join("");
+}
