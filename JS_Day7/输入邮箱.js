47ac9e036afd31c8f8aeb23e69069337b57c514b
Zhao Xiaoyan <3556555480@qq.com>
2017-04-28 10:23:02 +0800

    输入邮箱判断用户名、域名（slice）

--- JS_Day7/输入邮箱.js
+++ JS_Day7/输入邮箱.js
@@ -3,9 +3,12 @@ function mail(email)
   //工具：2个
   //indexOf查找字符的位置
   //slice从某个位置开始，截取到某位结束
-
+    var i = email.indexOf("@");
+    var userName = email.slice(0,i);
+    var domain = email.slice(i+1);   //从i+1开始到数组结束
+  
     alert("用户名"+username);
-    alert("域名"+doamin);
+    alert("域名"+domain);
 }
 
 
