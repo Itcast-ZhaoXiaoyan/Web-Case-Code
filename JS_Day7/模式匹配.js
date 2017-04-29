2f73622df91ba221adbf985f7c965bf325111100
Zhao Xiaoyan <3556555480@qq.com>
2017-04-29 09:14:35 +0800

    模式匹配match/replace/search

--- JS_Day7/模式匹配.js
+++ JS_Day7/模式匹配.js
@@ -1,12 +1,12 @@
 /*模式匹配*/
-var kword = "no" ;  //no,区分大小写，全部替换
+var kword = /no/ig;  //no,区分大小写，全部替换
 var reply =prompt("输入回复");
 
 //step1：查找所有匹配的关键字
 var arr = reply.match(kword);  //替换内容时，用match
 
 //step2：将所有no，替换为**
-reply = reply.replace(kword,"**");
+reply = reply.replace(kword,"**");   //kword为模式
 
 
 //step3：输出替换后结果，输出“共替换?处”
