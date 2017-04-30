40ed0777f01b049624f151798697c0031c0c140a
Zhao Xiaoyan <3556555480@qq.com>
2017-04-30 22:35:02 +0800

    对关键词敏感词的查找替换

---
+++ JS_Day8/敏感关键词StringRegAPI.js
@@ -0,0 +1,18 @@
+var str="纪委负责对全国党员干部的纪检监察工作";
+//纪委 纪检 纪检监察
+
+var reg=/纪[检委](监察)?/;
+var count=0;
+
+/*先找一次，如果找到
+    先判断当前关键字的字符个数，再根据个数替换*
+
+*/
+
+var arr=null;
+while( (arr=str.match(reg))!=null){
+    str=str.replace(reg,arr[0].length==2 ? "**" : "****");
+}
+
+console.log(str);
+console.log("共替换"+count);
