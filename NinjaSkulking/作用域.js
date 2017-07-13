fae1863ab0f0869b915c29686e13b544f27d499a
Zhao Xiaoyan <3556555480@qq.com>
2017-07-13 18:03:56 +0800

    Create 作用域.js

---
+++ NinjaSkulking/作用域.js
@@ -0,0 +1,6 @@
+Assert(true,”some descriptive text”);
+Assert(typeof outer ==’function’,”outer() is in scope”);
+Assert(typeof inner == ‘function’, “inner() is in scope”);
+Assert(typeof a === ‘number’,”a is in scope”);
+Assert(typeof b === ‘number’,”b is in scope”);
+Assert(typeof c === ‘number’,”c is in scope”);
